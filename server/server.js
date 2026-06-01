a2695245c3332bcfa815e4a9d9e73bdfb8f0bdb3
Adeel Hussain <adeel@adeelh.me>
2026-06-01 14:57:29 -0700

    Clean up

--- server/server.js
+++ server/server.js
@@ -8,16 +8,15 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-// This opens the WebSocket channel
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // This is your Vite React app's URL
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
 
-// The port we will run on
+
 const PORT = 3001;
 server.listen(PORT, () => {
-  console.log(`✅ Backend game server is running on port ${PORT}`);
+  console.log(`[Server] Running on port ${PORT}`);
 });
