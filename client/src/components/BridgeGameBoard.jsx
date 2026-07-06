200d08e4a85850068aa5c6df2d6f6e01a1293dbb
Adeel Hussain <adeel@adeelh.me>
2026-07-05 20:05:00 -0700

    renamed header -> bridgegameheader + inital set up

--- client/src/components/BridgeGameBoard.jsx
+++ client/src/components/BridgeGameBoard.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
+import BridgeGameHeader from "./BridgeGameHeader";
 
 export default function BridgeGameBoard({gameState}) {
-    
+
+    gameState.teamScore = 4;
+    gameState.enemyScore = 4;
+
+    return (
+        <div className="relative w-screen h-screen bg-[#0f172a]">
+            <BridgeGameHeader gameState={gameState}></BridgeGameHeader>
+
+        </div>
+    )
 }
