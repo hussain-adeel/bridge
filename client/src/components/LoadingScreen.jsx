724d330ddc32039ff73dd77121e849f37a1b7fab
Adeel Hussain <adeel@adeelh.me>
2026-07-14 17:45:27 -0700

    Added loading component/screen

--- client/src/components/LoadingScreen.jsx
+++ client/src/components/LoadingScreen.jsx
@@ -1,4 +1,9 @@
 export default function LoadingScreen({}) {
     
-    
+    return (
+        <div className="w-screen h-screen bg-board-bg flex flex-col items-center p-24">
+            <div className="w-10 h-10 md:w-15 md:h-15 border-4 border-gray-200 border-t-text-main rounded-full animate-spin"></div>
+            <div className="text-white font-bold text-3xl md:text-6xl mt-3">Loading</div>
+        </div>
+    )
 }
--- client/src/components/RoomManager.jsx
+++ client/src/components/RoomManager.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
+import LoadingScreen from "./LoadingScreen"
+import BridgeGameBoard from "./BridgeGameBoard";
+import Lobby from "./Lobby"
 
 export default function RoomManager({gameState, localUser}) {
+    if (!gameState) return <LoadingScreen />;
 
+    
 }
