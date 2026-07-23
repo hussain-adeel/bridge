fb198a77e41c1c90c5da95674cbd27d7f9d1a901
Adeel Hussain <adeel@adeelh.me>
2026-07-23 02:21:30 -0700

    Setting up RoomManager logic

--- client/src/components/RoomManager.jsx
+++ client/src/components/RoomManager.jsx
@@ -2,9 +2,27 @@ import { useState } from "react";
 import LoadingScreen from "./LoadingScreen"
 import BridgeGameBoard from "./BridgeGameBoard";
 import Lobby from "./Lobby"
+import { useParams } from "react-router-dom";
+
+const { roomCode } = useParams();
 
 export default function RoomManager({gameState, localUser}) {
-    if (!gameState) return <LoadingScreen />;
+    const safeGamePhase = gameState?.gamePhase ?? "Loading"
 
+
+    if (!gameState || safeGamePhase === "Loading") return <LoadingScreen />;
+    else if (safeGamePhase === "Lobby") 
+        return (
+            <Lobby 
+                roomCode={roomCode}
+            /> 
+        )
+    else {
+        return (
+            <BridgeGameBoard
+                gameState={gameState}
+            />
+        )
+    }
     
 }
