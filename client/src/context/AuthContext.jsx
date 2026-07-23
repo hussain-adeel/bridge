b6e6980178225a25fb1ba2be141bcc2ceefc8efe
Adeel Hussain <adeel@adeelh.me>
2026-07-23 01:56:33 -0700

    AuthContext (avoid prop drilling session)

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,51 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import { supabase } from "../utils/supabase";
+
+const AuthContext = createContext();
 
 export function AuthProvider({children}) {
-    
+    const [user, setUser] = useState(null);
+    const [session, setSession] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({data: {session}}) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        });
+
+        const { data: {subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        });
+
+        return () => subscription.unsubscribe();
+    })
+
+    const signOut = () => supabase.auth.signOut();
+
+    const value = {
+        user,
+        session,
+        loading,
+        signOut
+    };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+
 }
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+
+    if (context === undefined)
+        throw new Error('invalid usage of useAuth, must be used within AuthCotext')
+
+    return context;
+};
