6d8283066e517211b5bb73bc0573f66d36af1816
Adeel Hussain <adeel@adeelh.me>
2026-08-02 00:05:09 -0700

    fixed typo, added some error logging

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -8,10 +8,14 @@ const getSocketToken = async () => {
     return data.session?.access_token || null;
 }
 
-export const scoket = io(URL, {
+export const socket = io(URL, {
     autoConnect: true,
     auth: async (cb) => {
         const token = await getSocketToken();
         cb({ token });
     }
+});
+
+socket.on("connect_error", (err) => {
+    console.error("Socket Connection Failed:", err.message);
 });
