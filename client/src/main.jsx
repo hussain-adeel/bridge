4d512249a2a4542e88f3f04b46f8ca42a9d89359
Adeel Hussain <adeel@adeelh.me>
2026-08-02 00:04:34 -0700

    socket import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import "./utils/socket.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
